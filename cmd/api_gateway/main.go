3666849191bae79857410dcb0d8c0ea94de6b371
Minh <anhminh7802@gmail.com>
2026-02-19 13:55:45 -0700

    feat: implement a simple API Gateway with rate limiting and health check endpoint

--- cmd/api_gateway/main.go
+++ cmd/api_gateway/main.go
@@ -1,9 +1,40 @@
 package main
 
 import (
-	"github.com/anonymousminh/url_shortener/internal/logger"
+	"net/http"
+
+	"github.com/labstack/echo/v5"
+	"github.com/labstack/echo/v5/middleware"
 )
 
 func main() {
-	logger.Info("api_gateway", "Starting API Gateway...")
+
+	// Create a new Echo instance
+	e := echo.New()
+
+	// Configure the Rate Limter
+	rateLimitConfig := middleware.RateLimiterConfig{
+		// Store our own defined rate limit
+		Store: middleware.NewRateLimiterMemoryStore(20.0),
+		// DenyHandler to handle the error message if denied
+		DenyHandler: func(c *echo.Context, identifier string, err error) error {
+			return &echo.HTTPError{
+				Code:    middleware.ErrRateLimitExceeded.Code,
+				Message: middleware.ErrRateLimitExceeded.Message,
+			}
+		},
+	}
+
+	// Register middleware
+	e.Use(middleware.RateLimiterWithConfig(rateLimitConfig))
+
+	// Define a simple route called health
+	e.GET("/health", func(c *echo.Context) error {
+		return c.String(http.StatusOK, "API Gateway is healthy")
+	})
+
+	// Start server on port 8080
+	if err := e.Start(":8080"); err != nil {
+		e.Logger.Error("Failed to start the server", "error", err)
+	}
 }
